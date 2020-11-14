7aa52d9fdbd6c763aa6eaa1495600370cdc7b16a
Jeff Zhang <zhang.jeffreyd@gmail.com>
2020-11-14 16:42:40 -0500

    Render a few basic entities

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,72 @@
-use rltk::{Console, GameState, Rltk, RGB, VirtualKeyCode};
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
-use specs_derive::Component;
+use specs_derive::*;
 
-struct State {}
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB
+}
+
+struct State {
+    ecs: World
+}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+        
+        // asking the ECS for read-only access to Position and Renderable components
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
-fn main() {
+fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-
     let context = RltkBuilder::simple80x50()
         .with_title("Rust Roguelike")
-        .build();
-    let gs = State{};
+        .build()?;
+    let mut gs = State{
+        ecs: World::new()
+    };
 
-    rltk::main_loop(context, gs);
+    // tells our World to look at the types we give it and do some internal magicto create storage systems for each
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x : i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .build();
+    }
+
+    rltk::main_loop(context, gs)
 }
